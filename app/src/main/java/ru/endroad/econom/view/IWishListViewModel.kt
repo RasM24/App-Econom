b281b38ab6e8b50a1c0dd97e6d0015e34a7f47d4
RasM24 <dvolyatik@yandex.ru>
2019-12-26 20:17:58 +0700

    [feature.wishes] Создал модуль фичи с желаниями

--- app/src/main/java/ru/endroad/econom/view/IWishListViewModel.kt
+++ app/src/main/java/ru/endroad/econom/view/IWishListViewModel.kt
@@ -2,15 +2,16 @@ package ru.endroad.econom.view
 
 import androidx.lifecycle.LiveData
 import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.Job
 import ru.endroad.econom.entity.TotalResult
 import ru.endroad.econom.entity.Wish
 import ru.endroad.econom.presenter.WishList
 
-interface IWishListViewModel{
+interface IWishListViewModel {
 
 	val data: LiveData<WishList>
 	fun getInfo(wish: Wish)
 	fun perform(wish: Wish)
-	fun delete(wish: Wish)
+	fun delete(wish: Wish): Job
 	fun calculateEstimationAsync(sum: Int): Deferred<TotalResult>
 }
