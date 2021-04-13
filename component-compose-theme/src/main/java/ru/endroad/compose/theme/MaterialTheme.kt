ab8b630ede74971f5b6564fef2d51600362cb937
o.dvolyatik <o.dvolyatik@gmail.com>
2021-04-14 00:37:00 +0700

    [compose] Добавил MaterialTheme для JetpackCompose

---
+++ component-compose-theme/src/main/java/ru/endroad/compose/theme/MaterialTheme.kt
@@ -0,0 +1,15 @@
+package ru.endroad.compose.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ApplicationTheme(
+	darkTheme: Boolean = isSystemInDarkTheme(),
+	content: @Composable () -> Unit
+) = MaterialTheme(
+	colors = if (darkTheme) DarkThemeColors else LightThemeColors,
+	shapes = Shapes,
+	content = content
+)
