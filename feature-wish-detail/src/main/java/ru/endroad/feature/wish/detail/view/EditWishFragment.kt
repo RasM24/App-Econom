66fbf531a65f3046a03d99731a77c54c58b4f4c0
o.dvolyatik <o.dvolyatik@gmail.com>
2021-04-25 16:49:32 +0700

    [compose] Перевел фрагмент с редактированием/добавлением желания на Compose

--- feature-wish-detail/src/main/java/ru/endroad/feature/wish/detail/view/EditWishFragment.kt
+++ feature-wish-detail/src/main/java/ru/endroad/feature/wish/detail/view/EditWishFragment.kt
@@ -4,32 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.EditText
-import androidx.annotation.StringRes
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
-import com.google.android.material.textfield.TextInputLayout
-import kotlinx.android.synthetic.main.wish_edit_fragment.apply_button
-import kotlinx.android.synthetic.main.wish_edit_fragment.input_cost
-import kotlinx.android.synthetic.main.wish_edit_fragment.input_cost_layout
-import kotlinx.android.synthetic.main.wish_edit_fragment.input_important
-import kotlinx.android.synthetic.main.wish_edit_fragment.input_important_layout
-import kotlinx.android.synthetic.main.wish_edit_fragment.input_info
-import kotlinx.android.synthetic.main.wish_edit_fragment.input_name
-import kotlinx.android.synthetic.main.wish_edit_fragment.input_name_layout
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.koin.core.parameter.parametersOf
-import ru.endroad.feature.wish.detail.R
-import ru.endroad.feature.wish.detail.domain.ValidationResult
+import ru.endroad.component.core.setComposeView
 import ru.endroad.feature.wish.detail.presentation.EditScreenEvent
 import ru.endroad.feature.wish.detail.presentation.EditScreenState
 import ru.endroad.feature.wish.detail.presentation.EditWishViewModel
-import ru.endroad.shared.wish.core.entity.Importance
+import ru.endroad.shared.wish.core.entity.Wish
 
 class EditWishFragment : Fragment() {
 
@@ -37,89 +22,31 @@ class EditWishFragment : Fragment() {
 
 	private val presenter by viewModel<EditWishViewModel> { parametersOf(wishId) }
 
-	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
-		inflater.inflate(R.layout.wish_edit_fragment, container, false)
-
-	override fun onActivityCreated(savedInstanceState: Bundle?) {
-		super.onActivityCreated(savedInstanceState)
-
-		presenter.state.asStateFlow()
-			.onEach(::render)
-			.launchIn(lifecycleScope)
-
-		input_name.bindChangeFocus(EditScreenEvent::NameInputLostFocus, EditScreenEvent::NameInputReceiveFocus)
-		input_info.bindChangeFocus(EditScreenEvent::InfoInputLostFocus, EditScreenEvent::InfoInputReceiveFocus)
-		input_cost.bindChangeFocus(EditScreenEvent::CostInputLostFocus, EditScreenEvent::CostInputReceiveFocus)
-		input_important.bindChangeFocus(EditScreenEvent::ImportanceInputLostFocus, EditScreenEvent::ImportanceInputReceiveFocus)
-
-		apply_button.setOnClickListener {
-			presenter.reduce(
-				EditScreenEvent.ApplyClick(
-					name = input_name.text.toString(),
-					cost = input_cost.text.toString(),
-					importance = input_important.text.toString(),
-					info = input_info.text.toString()
-				)
-			)
-		}
-
-		//TODO вынести в presenter-слой
-		val importances = Importance.values().map(Importance::name)
-		val adapter = ArrayAdapter(requireContext(), R.layout.dropdown, importances)
-		input_important.setAdapter(adapter)
+	private val createWishFunction = { name: String, info: String, cost: String, importance: String ->
+		presenter.reduce(EditScreenEvent.ApplyClick(name = name, info = info, cost = cost, importance = importance))
 	}
 
-	private fun render(state: EditScreenState) {
-		when (state) {
-			EditScreenState.Initial            -> Unit
-			EditScreenState.InitialNewWish     -> renderNewWishScreen()
-			is EditScreenState.InitialEditWish -> renderEditWishScreen(state)
-			is EditScreenState.Validating      -> renderValidatingFieldsScreen(state)
-			EditScreenState.WishSaved          -> requireFragmentManager().popBackStack()
-		}
-	}
+	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View =
+		setComposeView {
+			val state = presenter.state.collectAsState()
 
-	private fun renderEditWishScreen(state: EditScreenState.InitialEditWish) {
-		requireActivity().title = getString(R.string.edit_screen_editing_wish)
-		apply_button.setText(R.string.edit_screen_edit_wish)
-
-		state.wish.run {
-			input_name.setText(name)
-			input_cost.setText("$cost")
-			input_info.setText(info)
-			input_important.setText(importance.name, false)
-		}
-	}
-
-	private fun renderNewWishScreen() {
-		requireActivity().title = getString(R.string.edit_screen_new_wish)
-		apply_button.setText(R.string.edit_screen_add_wish)
-	}
-
-	private fun renderValidatingFieldsScreen(state: EditScreenState.Validating) {
-		state.costField?.let { input_cost_layout.defineError(R.string.cost_input_error, it is ValidationResult.Invalid) }
-		state.importanceField?.let { input_important_layout.defineError(R.string.importance_input_error, it is ValidationResult.Invalid) }
-		state.nameField?.let {
-			input_name_layout.error = when (it) {
-				ru.endroad.feature.wish.detail.presentation.NameFieldValidate.EMPTY    -> resources.getString(R.string.name_input_error_empty)
-				ru.endroad.feature.wish.detail.presentation.NameFieldValidate.LONG     -> resources.getString(R.string.name_input_error_long)
-				ru.endroad.feature.wish.detail.presentation.NameFieldValidate.VALIDATE -> null
+			when (val renderedState = state.value) {
+				is EditScreenState.InitialEditWish -> RenderEditWish(renderedState.wish)
+				EditScreenState.InitialNewWish     -> RenderWishDetail(createWish = createWishFunction)
+				is EditScreenState.Validating      -> requireFragmentManager().popBackStack()
+				EditScreenState.WishSaved          -> requireFragmentManager().popBackStack()
 			}
 		}
-	}
 
-	private fun TextInputLayout.defineError(@StringRes textErrorId: Int, showError: Boolean) {
-		error = if (showError) resources.getString(textErrorId) else null
-	}
-
-	private fun EditText.bindChangeFocus(
-		onLostFocusEvent: (String) -> EditScreenEvent,
-		onReceiveFocusEvent: () -> EditScreenEvent
-	) {
-		setOnFocusChangeListener { _, isFocused ->
-			val event = if (isFocused) onReceiveFocusEvent() else onLostFocusEvent(text.toString())
-			presenter.reduce(event)
-		}
+	@Composable
+	private fun RenderEditWish(wish: Wish) {
+		RenderWishDetail(
+			nameDraft = wish.name,
+			infoDraft = wish.info,
+			costDraft = wish.cost.toString(),
+			importanceDraft = wish.importance.name,
+			createWish = createWishFunction
+		)
 	}
 
 	companion object {
