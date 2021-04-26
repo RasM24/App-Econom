73687c0fd35301a7391d4c74e0e0fd7444640937
o.dvolyatik <o.dvolyatik@gmail.com>
2021-04-27 00:18:21 +0700

    [compose] Добавил новые базовые composable

--- component-compose-theme/src/main/java/ru/endroad/composable/TextComposable.kt
+++ component-compose-theme/src/main/java/ru/endroad/composable/TextComposable.kt
@@ -5,6 +5,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import ru.endroad.compose.theme.MaterialColor
 import ru.endroad.compose.theme.s600
 
@@ -13,11 +14,13 @@ fun TitleText(
 	text: String,
 	modifier: Modifier = Modifier,
 	color: Color = Color.Unspecified,
+	textAlign: TextAlign? = null,
 ) = Text(
 	text = text,
 	modifier = modifier,
 	color = color,
 	style = MaterialTheme.typography.h6,
+	textAlign = textAlign,
 )
 
 @Composable
@@ -25,11 +28,13 @@ fun PrimaryText(
 	text: String,
 	modifier: Modifier = Modifier,
 	color: Color = Color.Unspecified,
+	textAlign: TextAlign? = null,
 ) = Text(
 	text = text,
 	modifier = modifier,
 	color = color,
 	style = MaterialTheme.typography.body1,
+	textAlign = textAlign,
 )
 
 @Composable
@@ -37,11 +42,13 @@ fun SecondaryText(
 	text: String,
 	modifier: Modifier = Modifier,
 	color: Color = Color.Unspecified,
+	textAlign: TextAlign? = null,
 ) = Text(
 	text = text,
 	modifier = modifier,
 	color = color,
 	style = MaterialTheme.typography.body2,
+	textAlign = textAlign,
 )
 
 @Composable
@@ -49,9 +56,11 @@ fun CaptionText(
 	text: String,
 	modifier: Modifier = Modifier,
 	color: Color = MaterialColor.Gray.s600, //TODO вынести в кастомную палитру
+	textAlign: TextAlign? = null,
 ) = Text(
 	text = text,
 	modifier = modifier,
 	color = color,
 	style = MaterialTheme.typography.caption,
+	textAlign = textAlign,
 )
