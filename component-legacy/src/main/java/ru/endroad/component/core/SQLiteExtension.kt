77e39e084e5243d3a5d460cbcbbd869e48598b39
o.dvolyatik <o.dvolyatik@gmail.com>
2021-05-20 23:50:28 +0700

    [cleaning] Пометил некоторые классы как deprecated

--- component-legacy/src/main/java/ru/endroad/component/core/SQLiteExtension.kt
+++ component-legacy/src/main/java/ru/endroad/component/core/SQLiteExtension.kt
@@ -3,6 +3,7 @@ package ru.endroad.component.core
 import androidx.room.RoomDatabase
 import androidx.sqlite.db.SupportSQLiteDatabase
 
+@Deprecated("Вынести")
 fun SupportSQLiteDatabase.runTransaction(transaction: SupportSQLiteDatabase.() -> Unit) {
 	try {
 		beginTransaction()
@@ -13,6 +14,7 @@ fun SupportSQLiteDatabase.runTransaction(transaction: SupportSQLiteDatabase.() -
 	}
 }
 
+@Deprecated("Вынести")
 fun <T : RoomDatabase> RoomDatabase.Builder<T>.preload(preloader: SupportSQLiteDatabase.() -> Unit): RoomDatabase.Builder<T> {
 	this.addCallback(object : RoomDatabase.Callback() {
 		override fun onCreate(db: SupportSQLiteDatabase) {
