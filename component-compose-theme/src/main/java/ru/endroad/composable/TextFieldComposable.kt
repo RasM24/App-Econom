b1423673f0f0ec12f6278dddc8da4df917705757
o.dvolyatik <o.dvolyatik@gmail.com>
2021-05-23 17:06:08 +0700

    [refactor] Вынес AutofillTextField в базовый модуль

--- component-compose-theme/src/main/java/ru/endroad/composable/TextFieldComposable.kt
+++ component-compose-theme/src/main/java/ru/endroad/composable/TextFieldComposable.kt
@@ -1,7 +1,9 @@
 package ru.endroad.composable
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.FocusInteraction
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsFocusedAsState
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
@@ -12,6 +14,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.ContentAlpha
+import androidx.compose.material.DropdownMenu
 import androidx.compose.material.Icon
 import androidx.compose.material.LocalContentAlpha
 import androidx.compose.material.LocalContentColor
@@ -21,6 +24,7 @@ import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.TextFieldColors
 import androidx.compose.material.TextFieldDefaults
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.ErrorOutline
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
@@ -28,11 +32,77 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.flow.collect
+
+@Composable
+fun AutofillTextField(
+	modifier: Modifier = Modifier,
+	value: TextFieldValue,
+	onValueChange: (TextFieldValue) -> Unit,
+	dischargeErrorState: () -> Unit,
+	enabled: Boolean = true,
+	textStyle: TextStyle = LocalTextStyle.current,
+	label: @Composable (() -> Unit)? = null,
+	helper: @Composable (BoxScope.() -> Unit)? = null,
+	leadingIcon: @Composable (() -> Unit)? = null,
+	suggestions: List<String>,
+	isError: Boolean = false,
+	visualTransformation: VisualTransformation = VisualTransformation.None,
+	keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+	keyboardActions: KeyboardActions = KeyboardActions(),
+	singleLine: Boolean = false,
+	maxLines: Int = Int.MAX_VALUE,
+	interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+	colors: TextFieldColors = TextFieldDefaults.outlinedTextFieldColors()
+) {
+	Box {
+		val expanded = interactionSource.collectIsFocusedAsState()
+		val focusManager = LocalFocusManager.current
+
+		MaterialTextField(
+			modifier = modifier,
+			value = value,
+			onValueChange = { onValueChange(it) },
+			dischargeErrorState = dischargeErrorState,
+			enabled = enabled,
+			readOnly = true,
+			textStyle = textStyle,
+			label = label,
+			helper = helper,
+			leadingIcon = leadingIcon,
+			trailingIcon =  { Icon(imageVector = Icons.Filled.ArrowDropDown, contentDescription = "choose") },
+			isError = isError,
+			visualTransformation = visualTransformation,
+			keyboardOptions = keyboardOptions,
+			keyboardActions = keyboardActions,
+			singleLine = singleLine,
+			maxLines = maxLines,
+			interactionSource = interactionSource,
+			colors = colors,
+		)
+
+		DropdownMenu(
+			expanded = expanded.value,
+			onDismissRequest = { focusManager.clearFocus() }
+		) {
+			suggestions.forEach { name ->
+				Box(modifier = Modifier
+					.fillMaxWidth()
+					.clickable {
+						onValueChange(TextFieldValue(text = name))
+						focusManager.clearFocus()
+					}) {
+					PrimaryText(modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp), text = name)
+				}
+			}
+		}
+	}
+}
 
 @Composable
 fun MaterialTextField(
