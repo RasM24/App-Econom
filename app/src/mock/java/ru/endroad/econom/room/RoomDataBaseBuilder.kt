9d9397b37ef6d9409b9bad23f56b2651ca129c78
o.dvolyatik <o.dvolyatik@gmail.com>
2021-05-23 22:16:05 +0700

    [refactor] Вынес метод предзаполнения базы данных

--- app/src/mock/java/ru/endroad/econom/room/RoomDataBaseBuilder.kt
+++ app/src/mock/java/ru/endroad/econom/room/RoomDataBaseBuilder.kt
@@ -2,10 +2,21 @@ package ru.endroad.econom.room
 
 import android.content.Context
 import androidx.room.Room
-import ru.endroad.component.core.preload
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
 import ru.endroad.econom.mock.fillWishTable
 
 fun buildDatabase(context: Context): AppDatabase =
 	Room.databaseBuilder(context, AppDatabase::class.java, DATABASE_NAME)
 		.preload { fillWishTable() }
 		.build()
+
+fun <T : RoomDatabase> RoomDatabase.Builder<T>.preload(preloader: SupportSQLiteDatabase.() -> Unit): RoomDatabase.Builder<T> {
+	this.addCallback(object : RoomDatabase.Callback() {
+		override fun onCreate(db: SupportSQLiteDatabase) {
+			db.preloader()
+		}
+	})
+
+	return this
+}
