2ac82e6cb9c83a9e2de1e5466895aff9cece4ffc
o.dvolyatik <o.dvolyatik@gmail.com>
2021-05-30 20:56:48 +0700

    [gradle] Перевел проект на Kotlin Gradle Script

---
+++ feature-wish-active/build.gradle.kts
@@ -0,0 +1,4 @@
+dependencies {
+	implementation(project(":shared-wish-core"))
+	implementation(project(":component-compose-theme"))
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+include(
+	":app",
+	":mock-wish",
+	":feature-wish-active",
+	":feature-wish-completed",
+	":feature-wish-detail",
+	":shared-wish-core",
+	":shared-wish-datasource",
+	":component-compose-theme",
+	":component-room"
+)
