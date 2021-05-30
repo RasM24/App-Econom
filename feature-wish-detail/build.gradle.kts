cf6483627a9c350c16ca9cbf4aaae9c482b59cb3
o.dvolyatik <o.dvolyatik@gmail.com>
2021-05-30 23:24:45 +0700

    [gradle] Разнес зависимости coroutines по модулям

--- feature-wish-detail/build.gradle.kts
+++ feature-wish-detail/build.gradle.kts
@@ -2,5 +2,7 @@ dependencies {
 	implementation(project(":shared-wish-core"))
 	implementation(project(":component-compose-theme"))
 
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.3")
+
 	implementation("org.koin:koin-core:2.1.5")
 }
