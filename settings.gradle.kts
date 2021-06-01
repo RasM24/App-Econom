b021db37e04cf69e1ed153218f57339246e4fb66
o.dvolyatik <o.dvolyatik@gmail.com>
2021-06-01 11:21:01 +0700

    [gradle] Добавил pluginManagement в settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,6 +12,24 @@ include(
 	":component-room"
 )
 
+pluginManagement {
+	repositories {
+		google()
+		jcenter()
+	}
+
+	resolutionStrategy {
+		eachPlugin {
+			val pluginId = requested.id.id
+
+			when {
+				pluginId.startsWith("org.jetbrains.kotlin") -> useVersion("1.4.32")
+				pluginId.startsWith("com.android.")         -> useModule("com.android.tools.build:gradle:7.0.0-alpha15")
+			}
+		}
+	}
+}
+
 dependencyResolutionManagement {
 	repositories {
 		google()
